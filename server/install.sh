cf8908e2b369755561923ae278ad6e53e36f196e
tim <tim@elec.ac.nz>
2023-09-22 13:58:29 +1200

    Netting a network working

--- server/install.sh
+++ server/install.sh
@@ -1,1 +1,1 @@
-rsync -rv --exclude=client_example --exclude=data --exclude="config/*.key" ../server tart@tart.elec.ac.nz:headscale
+rsync --verbose -rv --exclude=client_example --exclude=data --exclude="config/*.key" ../server tart@tart.elec.ac.nz:headscale
