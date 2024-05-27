84b313b7490954847d42fb9f3d3ba32772583d85
tim <tim@elec.ac.nz>
2024-05-28 11:59:49 +1200

    Add environment variables to the startup script

--- server/proxy/start.sh
+++ server/proxy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up # --authkey=${TAILSCALE_AUTHKEY}
+/app/tailscale up --hostname=${TS_HOSTNAME} --accept-dns=${TS_ACCEPT_DNS} --authkey=${TS_AUTHKEY} ${TS_EXTRA_ARGS} 
 which nginx
 nginx -g 'pid /tmp/nginx.pid; daemon off;'
