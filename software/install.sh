92ed7bb191e8836292403b88ee649133dfcf963a
Max Scheel <max@elec.ac.nz>
2025-07-13 22:57:43 +1200

    tidyup

--- software/install.sh
+++ software/install.sh
@@ -15,20 +15,10 @@ sudo usermod -aG docker $USER
 
 mkdir -p ~/telescope_config
 
-
-# Create .env file next to the docker-compose-telescope.yml
-echo "TS_HOSTNAME='MyNewTelescope'" > .env
-echo "LOGIN_PW='somethingSecret'" >> .env
-echo "SECRET_KEY='anotherSecretUsedForToken'" >> .env
-# Copy calibrated_antenna_positions.json to ~/telescope_config/calibrated_antenna_positions.json
-cp calibrated_antenna_positions.json ~/telescope_config/calibrated_antenna_positions.json
-# Edit and copy telescope_config.json to ~/telescope_config/telescope_config.json
-cp telescope_config.json ~/telescope_config/telescope_config.json
-
-echo "    sudo raspi-config -- enable SPI" 
 sudo reboot
 
-#Now set up tailscale onto the elec.ac.nz tailcale network. This is done using
+#Now set up tailscale onto the elec.ac.nz tailcale network. This is done using 
 
 echo "After Reboot, do the following:"
-echo "    sudo tailscale up"
+echo "    sudo raspi-config"
+echo "    sudo tailscale up   -- enable SPI"
